1541c1a3a5a1f7a7f4e353d8a252719936823853
DevMPoveaCL <marcopoveah@gmail.com>
2023-05-05 21:22:37 -0400

    nuevo proyecto ORM

---
+++ JAVA/JavaORM/otrareunionmas/src/main/java/com/ormspring/otrareunionmas/OtrareunionmasApplication.java
@@ -0,0 +1,13 @@
+package com.ormspring.otrareunionmas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtrareunionmasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtrareunionmasApplication.class, args);
+	}
+
+}
---
+++ JAVA/JavaORM/otrareunionmas/src/test/java/com/ormspring/otrareunionmas/OtrareunionmasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ormspring.otrareunionmas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtrareunionmasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
