69071504c886d991ab671ffda20b12f4813919f2
DevMPoveaCL <marcopoveah@gmail.com>
2023-05-13 23:30:43 -0400

    añade comentarios al código Contenedor.java

--- JAVA/POO/sOOper/Clase/Contenedor/Contenedor.java
+++ JAVA/POO/sOOper/Clase/Contenedor/Contenedor.java
@@ -8,11 +8,13 @@ import POO.sOOper.Interfaz.IProducto;
 
 public abstract class Contenedor implements IContenedor {
 
+    // atributos de la clase Contenedor
     private String referencia;
     private int alto;
     private int resistencia;
     private Set<IProducto> productos;
 
+    // constructor de la clase Contenedor
     public Contenedor(String referencia, int alto, int resistencia) {
         this.referencia = referencia;
         this.alto = alto;
@@ -20,21 +22,25 @@ public abstract class Contenedor implements IContenedor {
         productos = new HashSet<IProducto>();
     }
 
+    // método getter para la referencia del contenedor
     @Override
     public String getReferencia() {
         return referencia;
     }
 
+    // método para calcular el volumen del contenedor
     @Override
     public int getVolumen() {
         return alto * getSuperficie();
     }
 
+    // método para calcular el volumen disponible en el contenedor
     @Override
     public int volumenDisponible() {
         return getVolumen() - volumenOcupado();
     }
 
+    // método privado para calcular el volumen ocupado en el contenedor
     private int volumenOcupado() {
         int res = 0;
         for (IProducto p : productos) {
@@ -43,27 +49,33 @@ public abstract class Contenedor implements IContenedor {
         return res;
     }
 
+    // método getter para la resistencia del contenedor
     @Override
     public int getResistencia() {
         return resistencia;
     }
 
+    // método getter para la lista de productos del contenedor
     @Override
     public Set<IProducto> getProducto() {
         return productos;
     }
 
+    // método para añadir un producto al contenedor
     @Override
     public boolean meter(IProducto producto) {
         boolean resistenciaOk = resiste(producto);
         boolean volumenOk = producto.tengoEspacio(this);
         boolean compatibilidadOk = true;
 
+        // Comprobamos si el producto a añadir es compatible con los productos ya
+        // existentes en el contenedor.
         for (IProducto p : productos) {
             boolean compatibleOk = producto.esCompatible(p);
             compatibilidadOk &= compatibleOk;
         }
 
+        // Comprobamos si se cumplen las condiciones para añadir el producto.
         boolean acepta = resistenciaOk && volumenOk && compatibilidadOk;
         if (acepta) {
             productos.add(producto);
@@ -72,11 +84,13 @@ public abstract class Contenedor implements IContenedor {
         return acepta;
     }
 
+    // método para comprobar si el contenedor resiste un producto
     @Override
     public boolean resiste(IProducto producto) {
         return resistencia > producto.getPeso();
     }
 
+    // método para representar el contenedor como una cadena de texto
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder("Contenedor " + referencia + " [" + getTipo() + " ] (sup "
