666485970e76ea7a6eaa777377e038410b12b6c8
DevMPoveaCL <marcopoveah@gmail.com>
2023-05-13 23:33:09 -0400

    añade comentarios

--- JAVA/POO/sOOper/Clase/Producto/Alimentacion.java
+++ JAVA/POO/sOOper/Clase/Producto/Alimentacion.java
@@ -5,15 +5,21 @@ import POO.sOOper.Interfaz.IProducto;
 
 public class Alimentacion extends Producto {
 
+    // Constructor de la clase "Alimentacion" que llama al constructor de la clase
+    // "Producto".
     public Alimentacion(String referencia, int peso, int volumen) {
         super(referencia, peso, volumen);
     }
 
+    // Override del método "getCategoria()" de la clase base "Producto" para
+    // devolver la categoría "Alimentacion".
     @Override
     public Categoria getCategoria() {
         return Categoria.ALIMENTACION;
     }
 
+    // Verifica si el producto es compatible con la alimentación, es decir, si es
+    // también de la categoría "Alimentacion".
     @Override
     public boolean esCompatible(IProducto p) {
         return Categoria.ALIMENTACION.equals(p.getCategoria());
