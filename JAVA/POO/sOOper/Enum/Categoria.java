685af6f72e4bb316a26dbe0167f89c2f0b8d6137
DevMPoveaCL <marcopoveah@gmail.com>
2023-05-13 23:48:08 -0400

    añade comentarios, carpeta Enum

--- JAVA/POO/sOOper/Enum/Categoria.java
+++ JAVA/POO/sOOper/Enum/Categoria.java
@@ -1,5 +1,6 @@
 package POO.sOOper.Enum;
 
 public enum Categoria {
+    // "Enum" (enumercaciones) = definen constantes relacionadas entre sí
     ALIMENTACION, DROGERIA, HIGIENE, MASCOTA;
 }
--- JAVA/POO/sOOper/Enum/TipoContenedor.java
+++ JAVA/POO/sOOper/Enum/TipoContenedor.java
@@ -1,5 +1,6 @@
 package POO.sOOper.Enum;
 
 public enum TipoContenedor {
+    // "Enum" (enumercaciones) = definen constantes relacionadas entre sí
     BOLSA, CAJA;
 }
