94737ffead21ef0685ba801c01359e17f0755cfa
DevMPoveaCL <marcopoveah@gmail.com>
2023-06-04 19:53:58 -0400

    Curso de Java Completo (+127hrs)

---
+++ JAVA/Java127Hrs/Java_127hrs/src/Java127Hrs.java
@@ -0,0 +1,27 @@
+public class Java127Hrs {
+    public static void main(String[] args) {
+        String saludar = "Curso de Java completo(+127hrs), vamos a darle con todo!";
+        System.out.println("saludar.toUpperCase() = " + saludar.toUpperCase());
+
+        int numero = 31;
+        boolean valor = true;
+        int numero2 = 5;
+
+        if(valor) {
+            System.out.println("numero = " + numero);
+            numero2 = 10;
+        }
+        System.out.println("numero2 = " + numero2);
+
+        var numero3 ="15";
+
+        String nombre;
+        nombre  = "Andrés";
+
+        if (numero > 10) {
+            nombre = "Juan";
+        }
+
+        System.out.println("nombre = " + nombre);
+    }
+}
