0791410240b390b5df0ddf1452b603da93edad91
DevMPoveaCL <marcopoveah@gmail.com>
2023-06-07 22:44:10 -0400

    agrega valores max y min de float y double

--- JAVA/Java127Hrs/Java127Hrs/src/Primitivos.java
+++ JAVA/Java127Hrs/Java127Hrs/src/Primitivos.java
@@ -5,32 +5,32 @@ public class Primitivos {
         //Tipo byte: 127(máx) y -128(min)
         byte numeroByte = 127;
         System.out.println("numeroByte = " + numeroByte);
-        System.out.println("Tipo byte corresponde en byte a: " + Byte.BYTES);
-        System.out.println("Tipo byte corresponde en bites a: " + Byte.SIZE);
+        System.out.println("Tipo byte corresponde en byte a: " + Byte.BYTES); //1
+        System.out.println("Tipo byte corresponde en bites a: " + Byte.SIZE); //8
         System.out.println("Valor máximo de un byte: " + Byte.MAX_VALUE);
         System.out.println("Valor mínimo de un byte: " + Byte.MIN_VALUE);
 
         //Tipo short: 32767(máx) y -32768(min)
         short numeroShort = 32767;
         System.out.println("\nnumeroShort = " + numeroShort);
-        System.out.println("Tipo short corresponde en byte a: " + Short.BYTES);
-        System.out.println("Tipo short corresponde en bites a: " + Short.SIZE);
+        System.out.println("Tipo short corresponde en byte a: " + Short.BYTES); //2
+        System.out.println("Tipo short corresponde en bites a: " + Short.SIZE); //16
         System.out.println("Valor máximo de un short: " + Short.MAX_VALUE);
         System.out.println("Valor mínimo de un short: " + Short.MIN_VALUE);
 
         //Tipo int: 2147483647(máx) y -2147483648(min).
         int numeroInt = 2147483647;
         System.out.println("\nnumeroInt = " + numeroInt);
-        System.out.println("Tipo int corresponde en byte a: " + Integer.BYTES);
-        System.out.println("Tipo int corresponde en bites a: " + Integer.SIZE);
+        System.out.println("Tipo int corresponde en byte a: " + Integer.BYTES); //4
+        System.out.println("Tipo int corresponde en bites a: " + Integer.SIZE); //32
         System.out.println("Valor máximo de un int: " + Integer.MAX_VALUE);
         System.out.println("Valor mínimo de un int: " + Integer.MIN_VALUE);
 
         // Tipo long: 9223372036854775807(máx) y -9223372036854775808(min)
         long numeroLong = 9223372036854775807L; //Buena práctica = mayus en lugar de minus para no confundir con un 1
         System.out.println("\nnumeroLong = " + numeroLong);
-        System.out.println("Tipo long corresponde en byte a: " + Long.BYTES);
-        System.out.println("Tipo long corresponde en bites a: " + Long.SIZE);
+        System.out.println("Tipo long corresponde en byte a: " + Long.BYTES); //8
+        System.out.println("Tipo long corresponde en bites a: " + Long.SIZE); //64
         System.out.println("Valor máximo de un long: " + Long.MAX_VALUE);
         System.out.println("Valor mínimo de un long: " + Long.MIN_VALUE);
 
@@ -42,7 +42,18 @@ public class Primitivos {
         float expNegativo = 0.00000000015f; //1.5e-10f
         System.out.println("expNegativo = " + expNegativo);
 
+        System.out.println("Tipo float corresponde en byte a: " + Float.BYTES); //4
+        System.out.println("Tipo float corresponde en bites a: " + Float.SIZE); //32
+        System.out.println("Valor máximo de un float: " + Float.MAX_VALUE); //3.4028235E38
+        System.out.println("Valor mínimo de un float: " + Float.MIN_VALUE); //1.4E-45
 
+        //Misma lógica que el tipo float en cuanto a los exponentes y letras (sufijo "D" opcional)
+        double realDouble = 3.14;
+        System.out.println("\nrealDouble = " + realDouble);
+        System.out.println("Tipo double corresponde en byte a: " + Double.BYTES); //8
+        System.out.println("Tipo double corresponde en bites a: " + Double.SIZE); //64
+        System.out.println("Valor máximo de un double: " + Double.MAX_VALUE); //1.7976931348623157E308
+        System.out.println("Valor mínimo de un double: " + Double.MIN_VALUE); //4.9E-324
 
         // var por defecto es int
         var intVar = 125;
