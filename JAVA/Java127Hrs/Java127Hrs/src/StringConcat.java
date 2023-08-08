c76b3b7236e5e027338f246dea5de7bb2019de09
DevMPoveaCL <marcopoveah@gmail.com>
2023-08-08 15:55:53 -0400

    agrega eficiencia de los métodos de concatenación

--- JAVA/Java127Hrs/Java127Hrs/src/StringConcat.java
+++ JAVA/Java127Hrs/Java127Hrs/src/StringConcat.java
@@ -19,8 +19,31 @@ public class StringConcat {
         System.out.println(numeroA + numeroB + " " + detalle);
 
 
-        //.concat es más eficiente que el signo "+", ya que no crea instancias builder por detrás
+        //si son pocos elementos ".concat" es más eficiente que el signo "+", ya que no crea instancias builder por detrás
         String detalle2 = curso.concat(" con ").concat(profesor);
         System.out.println("detalle2 = " + detalle2);
+
+        //Aunque si utilizamos directamente el StringBuilder para concatenar...
+        String a = "a";
+        String b = "b";
+        String c = a;
+
+        //StringBuilder sería el más eficiente sin importar la cantidad de elementos a concatenar.
+        StringBuilder sb = new StringBuilder(a);
+
+        long inicio = System.currentTimeMillis();
+
+        //tiempo determinado por cantidad de iteraciones, eficiencia de los 3 métodos demostrada por el for
+        for (int i = 0; i < 100000; i++){
+            // c = c.concat(a).concat(b).concat("\n"); // 500 => 2ms, 1000 => 4ms, 10000 => 105ms, 100000 => 5071ms
+            // c += a + b + "\n"; //500 => 19ms, 1000 => 21ms, 10000 => 68ms, 100000 =>1976ms
+            sb.append(a).append(b).append("\n"); //500 => 0ms, 1000 => 0ms, 10000 => 2ms, 100000 => 8ms
+        }
+
+        long fin =System.currentTimeMillis();
+        System.out.println(fin - inicio);
+        System.out.println("\nc = " + c);
+        System.out.println("sb = " + sb.toString());
+
     }
 }
