ce79ad0386346d1320f1aa274827b7239b95f1df
DevMPoveaCL <marcopoveah@gmail.com>
2024-05-29 15:29:24 -0400

    Añade múltiples excepciones

--- JAVA/Java127Hrs/poo_excepciones/src/org/mpovea/pooexcepciones/ejemplo/EjemploExcepciones.java
+++ JAVA/Java127Hrs/poo_excepciones/src/org/mpovea/pooexcepciones/ejemplo/EjemploExcepciones.java
@@ -1,5 +1,7 @@
 package org.mpovea.pooexcepciones.ejemplo;
 
+import javax.swing.*;
+
 public class EjemploExcepciones {
     public static void main(String[] args) {
 
@@ -11,11 +13,22 @@ public class EjemploExcepciones {
         System.out.println("Capturamos la excepción en tiempo de ejecución: " + e.getMessage());
         }
         System.out.println("Continuamos con el flujo de nuestra aplicación!" );*/
-        try {
-            int division = 10 / 0;
+
+        String valor = JOptionPane.showInputDialog("Ingrese un entero: "); // valor no numérico
+
+        try { //Todo lo que se quiere manejar en las excepciones.
+            int divisor = Integer.parseInt(valor); // Transforma a valor numérico el String anterior
+            int division = 10 / divisor;
             System.out.println(division);
-        } catch (ArithmeticException ae) { //resultado no cambia, pero en la práctica si es más específico.
+        } catch (NumberFormatException nfe){ //Tipo de excepción, msje y qué hacer.
+            System.out.println("Se detectó una excepción: Por favor, ingrese un valor numérico: " + nfe.getMessage());
+            main(args);
+        }
+        catch (ArithmeticException ae) { //resultado no cambia, pero en la práctica si es más específico.
             System.out.println("Capturamos la excepción en tiempo de ejecución: " + ae.getMessage());
+            main(args);
+        }finally { //Tarea que se ejecuta siempre independiente de que ocurra o no el error
+            System.out.println("Ejemplo: finalizar una conexión cuando ocurre el error, opcional");
         }
         System.out.println("Continuamos con el flujo de nuestra aplicación!" );
     }
