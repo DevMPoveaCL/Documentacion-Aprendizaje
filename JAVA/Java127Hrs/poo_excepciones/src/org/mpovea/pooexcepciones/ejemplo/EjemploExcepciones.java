fa1f2cf0f6b0df56d4965abb81c13009be5f5306
DevMPoveaCL <marcopoveah@gmail.com>
2024-05-29 15:16:47 -0400

    Actualiza try catch de Exception hacia ArithmeticException

--- JAVA/Java127Hrs/poo_excepciones/src/org/mpovea/pooexcepciones/ejemplo/EjemploExcepciones.java
+++ JAVA/Java127Hrs/poo_excepciones/src/org/mpovea/pooexcepciones/ejemplo/EjemploExcepciones.java
@@ -3,12 +3,19 @@ package org.mpovea.pooexcepciones.ejemplo;
 public class EjemploExcepciones {
     public static void main(String[] args) {
 
-        try {
+        /*try {
             int division = 10 / 0;
             System.out.println(division);
         } catch (Exception e) { //No es recomendable hacerlo tan general, se debe manejar la excepción por su tipo
                                 // en lugar de "Exception".
         System.out.println("Capturamos la excepción en tiempo de ejecución: " + e.getMessage());
+        }
+        System.out.println("Continuamos con el flujo de nuestra aplicación!" );*/
+        try {
+            int division = 10 / 0;
+            System.out.println(division);
+        } catch (ArithmeticException ae) { //resultado no cambia, pero en la práctica si es más específico.
+            System.out.println("Capturamos la excepción en tiempo de ejecución: " + ae.getMessage());
         }
         System.out.println("Continuamos con el flujo de nuestra aplicación!" );
     }
