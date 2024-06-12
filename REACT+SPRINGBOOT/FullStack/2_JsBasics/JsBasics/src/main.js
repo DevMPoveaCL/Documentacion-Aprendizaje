1a3fbd0bdb3e8f63cf5d68975ad8f5e56dde98bd
DevMPoveaCL <marcopoveah@gmail.com>
2024-06-11 22:33:12 -0400

    Agrega función tipo flecha

--- REACT+SPRINGBOOT/FullStack/2_JsBasics/JsBasics/src/main.js
+++ REACT+SPRINGBOOT/FullStack/2_JsBasics/JsBasics/src/main.js
@@ -1,12 +1,11 @@
-function sayHello(firstname = 'Pepe', age = 0){ //Pepe y "cero" es default para undefined.
-   
-   const greeting = `Hola mundo function: 
-   Nombre: ${firstname}
-   Edad: ${age}  `;
-    // console.log('Hola mundo function');
-    return greeting;
-}
+
+//Pepe y "cero" es default para undefined.
+const sayHello = (firstname = 'Pepe', age = 0) => `Hola mundo function= Nombre: ${firstname} Edad: ${age}  `;
+const add = (a = 0, b = 0) => a+b;
+// console.log('Hola mundo function');
+
 
 const result = sayHello('Andrés');
 
 console.log(result);
+console.log (add(10,5))
