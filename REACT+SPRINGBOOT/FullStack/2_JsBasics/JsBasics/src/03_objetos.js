5cb4db6545189990000bd56b5b9a9b592fc87584
DevMPoveaCL <marcopoveah@gmail.com>
2024-06-13 21:09:17 -0400

    Añade spread, chaining y ternario

---
+++ REACT+SPRINGBOOT/FullStack/2_JsBasics/JsBasics/src/03_objetos.js
@@ -0,0 +1,46 @@
+const invoice = {
+    id: 10,
+    name: 'Compras de Oficina',
+    date: new Date(),
+    client: {
+        id: 2,
+        firstname: 'John Doe',
+        lastname: 'Doe',
+        age: 20,
+    },
+    items: [
+        {
+            product: 'teclado',
+            price: 399,
+            quantity:2,
+        },
+        {
+            product: 'mouse',
+            price: 200,
+            quantity:1,
+        },
+        {
+            product: 'papel',
+            price: 100,
+            quantity:10,
+        },
+    ],
+    total: function() {
+        let total = 0;
+        this.items.forEach(item =>{
+            total = total + item.price*item.quantity;
+        });
+        return total;
+    },
+    greeting: function() {
+        return `Hola ${this.client.firstname}`;
+    }
+};
+
+// invoice.client.firstname = 'Pepe'; //Es como el método set de Java
+// invoice.total = 5000;
+console.log (invoice); // Se selecciona mediante punto si se quiere mostrar solo un atributo del objeto.
+
+const greeting = invoice.greeting();
+console.log(greeting);
+console.log('Total: ' + invoice.total());
--- REACT+SPRINGBOOT/FullStack/2_JsBasics/JsBasics/src/main.js
+++ REACT+SPRINGBOOT/FullStack/2_JsBasics/JsBasics/src/main.js
@@ -1,46 +1,17 @@
-const invoice = {
-    id: 10,
-    name: 'Compras de Oficina',
-    date: new Date(),
-    client: {
-        id: 2,
-        firstname: 'John Doe',
-        lastname: 'Doe',
-        age: 20,
-    },
-    items: [
-        {
-            product: 'teclado',
-            price: 399,
-            quantity:2,
-        },
-        {
-            product: 'mouse',
-            price: 200,
-            quantity:1,
-        },
-        {
-            product: 'papel',
-            price: 100,
-            quantity:10,
-        },
-    ],
-    total: function() {
-        let total = 0;
-        this.items.forEach(item =>{
-            total = total + item.price*item.quantity;
-        });
-        return total;
-    },
-    greeting: function() {
-        return `Hola ${this.client.firstname}`;
-    }
-};
 
-// invoice.client.firstname = 'Pepe'; //Es como el método set de Java
-// invoice.total = 5000;
-console.log (invoice); // Se selecciona mediante punto si se quiere mostrar solo un atributo del objeto.
 
-const greeting = invoice.greeting();
-console.log(greeting);
-console.log('Total: ' + invoice.total());
+const average = 5.9;
+let result = '';
+result = (average >= 4.0 ) ? 'Aprobado' : 'Rechazado';
+console.log(`Resultado: ${result}`);
+
+
+let max = 0;
+const a = 5;
+const b = 8;
+const c = 3;
+
+max = a > b ? a : b;
+max = max > c ? max : c;
+
+console.log (`El número mayor es: ${max}`);
