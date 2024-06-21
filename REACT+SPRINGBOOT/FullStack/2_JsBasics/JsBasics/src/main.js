a6cefb790a789a1e4cad3c32c511886decc2fb8e
DevMPoveaCL <marcopoveah@gmail.com>
2024-06-20 22:09:25 -0400

    Añade promesas then y catch

--- REACT+SPRINGBOOT/FullStack/2_JsBasics/JsBasics/src/main.js
+++ REACT+SPRINGBOOT/FullStack/2_JsBasics/JsBasics/src/main.js
@@ -4,9 +4,18 @@ import {invoiceById} from './data/invoices';
 //Asíncrono "simula API remoto "
 const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
-        const result = invoiceById(3);
-        console.log(result)
+        const result = invoiceById(2);
+        if (result){
+            resolve(result);
+        }else{
+            reject ('error: no existe la factura por el id!')
+        }
     }, 2500);
 }); 
 
-
+promise.then((json) => {
+    console.log(json);
+    console.log ('realizada con éxito alguna tarea con demora');
+}).catch((error) => {
+    console.error (error);
+});
