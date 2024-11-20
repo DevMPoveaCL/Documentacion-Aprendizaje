ab491c8326d1a176a879492d30ad190e5cb13f7c
judith povea <marcopoveah@gmail.com>
2024-11-20 19:23:49 -0300

    Agrega propTypes a la factura

--- REACT+SPRINGBOOT/FullStack/5_invoice-app/src/components/ClientView.jsx
+++ REACT+SPRINGBOOT/FullStack/5_invoice-app/src/components/ClientView.jsx
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types';
+
 export const ClientView = ({title, client}) => {
 
     const { name: nameClient, lastName, address } = client;
@@ -13,4 +15,9 @@ export const ClientView = ({title, client}) => {
             </ul>
         </>
     )
+}
+
+ClientView.propTypes = {
+    title: PropTypes.string.isRequired,
+    client: PropTypes.object.isRequired,
 }
